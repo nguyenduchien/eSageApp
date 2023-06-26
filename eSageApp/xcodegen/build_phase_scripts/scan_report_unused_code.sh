5a4588fd9261b183c27c9f3100d284f656cc10dc
Hien Nguyen <nduchien@icloud.com>
2023-06-26 16:00:27 +0700

    implement scrip Periphery by xcodegen ignore file xcodeproj in gỉt

---
+++ eSageApp/xcodegen/build_phase_scripts/scan_report_unused_code.sh
@@ -0,0 +1,20 @@
+# Type a script or drag a script file from your workspace to insert its path.
+SCHEME_NAME=`cat ${SCHEME_FILE}`
+echo "Current scheme: $SCHEME_NAME"
+
+rm ${SCHEME_FILE}
+
+if test -d /opt/homebrew/bin; then
+  PATH=/opt/homebrew/bin/:$PATH
+fi
+
+report_file="$SRCROOT/../unused_code_report.log"
+
+if which periphery >/dev/null; then
+  periphery clear-cache
+  periphery scan --workspace "../eSage.xcworkspace" --schemes "eSageAppStg" --targets "eSageApp" --format xcode --clean-build 2>&1 | tee -a $report_file
+else
+  echo "Periphery does not exist, download from https://github.com/peripheryapp/periphery"
+fi
+
+rm -f $file_path
